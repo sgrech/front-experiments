2f6eaaeead8aa9a4212c7b895d2abc774bf2bcb3
Shane Grech <shanegrech@gmail.com>
2021-01-27 16:29:59 +0100

    Adding handlebars example

---
+++ webpack-handlebars-landing/webpack.config.js
@@ -0,0 +1,104 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  entry: {
+    bundle: "./src/app.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+  },
+  devtool: isDevelopment && "source-map",
+  devServer: {
+    port: 3000,
+    open: true,
+    contentBase: path.join(__dirname, "../src"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.handlebars$/,
+        use: [
+          { loader: "handlebars-loader" },
+          {
+            loader: "string-replace-loader",
+            options: {
+              search: "# vi: ft=html\n",
+              replace: "",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: ["autoprefixer"],
+              },
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "static",
+              publicPath: "./static",
+              useRelativePath: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    /** Since Webpack 4 */
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        handlebarsLoader: {},
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-styles.css",
+      chunkFilename: "[id].css",
+    }),
+
+    new HtmlWebpackPlugin({
+      title: "My awesome service",
+      template: "./src/index.handlebars",
+      minify: !isDevelopment && {
+        html5: true,
+        collapseWhitespace: true,
+        caseSensitive: true,
+        removeComments: true,
+        removeEmptyElements: true,
+      },
+    }),
+  ],
+};
